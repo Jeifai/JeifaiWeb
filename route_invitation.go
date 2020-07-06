79b3fe1974b679c3a4f764bf49afe37388de9fe1
robimalco <robimalco@gmail.com>
2020-07-06 14:46:50 +0200

    Update top bar #7

--- route_invitation.go
+++ route_invitation.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
-	"net/http"
+    "net/http"
+    "encoding/json"
 )
 
 func invitation(w http.ResponseWriter, r *http.Request) {
@@ -14,39 +14,39 @@ func invitation(w http.ResponseWriter, r *http.Request) {
 }
 
 func submitInvitation(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Starting submitInvitation...")
+    fmt.Println("Starting submitInvitation...")
 	var invitation Invitation
-	err := json.NewDecoder(r.Body).Decode(&invitation)
+    err := json.NewDecoder(r.Body).Decode(&invitation)
 	if err != nil {
 		panic(err.Error())
-	}
+    }
 
-	invitation.InvitationByEmail()
+    invitation.InvitationByEmail()
 
-	var messages []string
+    var messages []string
 
-	if invitation.Id == 0 {
-		invitation.CreateInvitation()
+    if invitation.Id == 0 {
+        invitation.CreateInvitation()
 		green_1 := `<p style="color:green">`
-		green_2 := `</p>`
-		temp_message := green_1 + "Information has been correctely recevied!" + green_2
-		messages = append(messages, temp_message)
+        green_2 := `</p>`
+        temp_message := green_1  +"Information has been correctely recevied!" + green_2
+        messages = append(messages, temp_message)
+        
+        // SEND EMAIL CONFIRMATION
 
-		// SEND EMAIL CONFIRMATION
-
-	} else {
-		red_1 := `<p style="color:green">`
+    } else {
+		red_1 := `<p style="color:red">`
 		red_2 := `</p>`
-		temp_message := red_1 + "Ouch, something got wrong. Please Write us at robimalco@gmail.com!" + red_2
-		messages = append(messages, temp_message)
-	}
+        temp_message := red_1 + "Ouch, something got wrong. Please write at robimalco@gmail.com" + red_2
+        messages = append(messages, temp_message)
+    }
 
 	type TempStruct struct {
 		Messages []string
-	}
+    }
 
 	infos := TempStruct{messages}
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+    w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(infos)
 }
