3ae711cb49e19d8d59913250d56baf1d9020f3b7
robimalco <robimalco@gmail.com>
2020-07-07 07:24:11 +0200

    #9 Write query to check UUID by email

--- db_invitation.go
+++ db_invitation.go
@@ -6,19 +6,32 @@ import (
 )
 
 type Invitation struct {
-	Id             int
-	Email          string
-	Whyjoin        string
-	Whichcompanies string
-	Anythingelse   string
+    Id              int
+    Uuid            string
+	Email           string
+	Whyjoin         string
+	Whichcompanies  string
+	Anythingelse    string
 }
 
-func (invitation *Invitation) InvitationByEmail() (err error) {
-	fmt.Println("Starting InvitationByEmail...")
+func (invitation *Invitation) InvitationIdByEmail() (err error) {
+	fmt.Println("Starting InvitationIdByEmail...")
 	err = Db.QueryRow(`SELECT
                          i.id
                        FROM invitations i
                        WHERE i.email=$1`, invitation.Email).Scan(&invitation.Id)
+	return
+}
+
+func (invitation *Invitation) InvitationIdByUuidAndEmail() (err error) {
+	fmt.Println("Starting InvitationIdByUuidAndEmail...")
+    err = Db.QueryRow(`SELECT i.id
+                       FROM invitations i
+                       WHERE i.uuid=$1
+                       AND i.email=$2 
+                       AND i.usedat IS NULL`, 
+        invitation.Uuid, 
+        invitation.Email).Scan(&invitation.Id)
 	return
 }
 
@@ -39,4 +52,17 @@ func (invitation *Invitation) CreateInvitation() {
 		invitation.Anythingelse,
 		time.Now(),
 	)
+}
+
+func (invitation *Invitation) UpdateInvitation() {
+	statement := `UPDATE invitations SET usedat = current_timestamp WHERE id=$1;`
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+    _, err = stmt.Exec(invitation.Id)
+    if err != nil {
+		panic(err.Error())
+    }
 }
