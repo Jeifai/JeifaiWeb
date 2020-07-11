3d32fb6865bc10cd45d1e68cd1a08cef807f3c08
robimalco <robimalco@gmail.com>
2020-07-11 16:48:46 +0200

    import library and structure #19

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/lib/pq v1.7.0
 	github.com/logrusorgru/aurora v2.0.3+incompatible
+	github.com/sirupsen/logrus v1.6.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	mvdan.cc/gofumpt v0.0.0-20200627213337-90206bd98491 // indirect
 )
---
+++ init.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/dchest/uniuri"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	. "github.com/logrusorgru/aurora"
+	"github.com/sirupsen/logrus"
+)
+
+const isLocal = true
+
+var log = logrus.New()
+
+var Db *sql.DB
+
+func init() {
+	init_log()
+	init_db()
+}
+
+func init_log() {
+	file, err := os.OpenFile("logrus.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
+	if err == nil {
+		log.Out = file
+	} else {
+		log.Info("Failed to log to file, using default stderr")
+	}
+
+	log.SetFormatter(&logrus.JSONFormatter{})
+
+	log.WithFields(logrus.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
+}
+
+func createUUID() (uuid string) {
+	u := new([16]byte)
+	_, err := rand.Read(u[:])
+	if err != nil {
+		return
+	}
+
+	// 0x40 is reserved variant from RFC 4122
+	u[8] = (u[8] | 0x40) & 0x7F
+	// Set the four most significant bits (bits 12 through 15) of the
+	// time_hi_and_version field to the 4-bit version number.
+	u[6] = (u[6] & 0xF) | (0x4 << 4)
+	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+	return
+}
+
+func init_db() {
+	if isLocal {
+		err := godotenv.Load()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	psqlInfo := os.Getenv("POSTGRES_CONNECTION")
+	db, err := sql.Open("postgres", psqlInfo)
+	Db = db
+	if err != nil {
+		panic(err.Error())
+	}
+	if err = Db.Ping(); err != nil {
+		Db.Close()
+		fmt.Println(Red("Unsuccessfully connected to the database"))
+		return
+	}
+	fmt.Println(Green("Successfully connected to the database"))
+}
+
+func Encrypt(plaintext string) (cryptext string) {
+	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return
+}
+
+func GenerateToken() (token string) {
+	token = uniuri.NewLen(40)
+	return
+}
